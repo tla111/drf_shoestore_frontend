a5aac0a76ac4fcd07f75c48facfc5f8a383110d1
Tim La <email@example.com>
2020-12-24 18:06:00 -0500

    properly connected backend to frontend, displaying shoe information

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  const [shoeList, setShoeList] = useState([
-    { name: 'Nike', id: 1 },
-    { name: 'Jordan', id: 2 },
-    { name: 'Toms', id: 3 },
-  ]);
+  const [shoeList, setShoeList] = useState([]);
 
-  // useEffect(() => {
-  //   setShoeList()
-  // }, [shoeList]);
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:8000/api/shoe/')
+      .then((res) => setShoeList(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div>
       <ul>
         {shoeList.map((shoe) => (
-          <li key={shoe.id}>{shoe.name}</li>
+          <div key={shoe.id}>
+            <li>Brand Name: {shoe.brand_name}</li>
+            <li>Color: {shoe.color}</li>
+            <li>Fasten Type: {shoe.fasten_type}</li>
+            <li>Manufacturer: {shoe.manufacturer}</li>
+            <li>Material: {shoe.material}</li>
+            <li>Shoe Type: {shoe.shoe_type}</li>
+            <li>Size: {shoe.size}</li>
+          </div>
         ))}
       </ul>
     </div>
